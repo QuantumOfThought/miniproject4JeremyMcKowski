0ac7aa935d20e832c2726ef5db87e05053dbaaee
FHSU-dude <188289469+QuantumOfThought@users.noreply.github.com>
2025-11-10 23:39:15 -0600

    Add add, edit, and delete movie views and templates

--- MoviesCollection/MoviesCollection/urls.py
+++ MoviesCollection/MoviesCollection/urls.py
@@ -5,4 +5,7 @@ from movies import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
+    path("add/", views.add_movie, name="add_movie"),
+    path("delete/<int:movie_id>/", views.delete_movie, name="delete_movie"),
+    path("edit/<int:movie_id>/", views.edit_movie, name="edit_movie"),
 ]
--- MoviesCollection/movies/views.py
+++ MoviesCollection/movies/views.py
@@ -4,3 +4,37 @@ from .models import Movie
 def home(request):
     movies = Movie.objects.all()
     return render(request, "movies/home.html", {"movies": movies})
+
+from django.shortcuts import render, redirect
+from .forms import MovieForm
+
+def add_movie(request):
+    if request.method == "POST":
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = MovieForm()
+    return render(request, "movies/add.html", {"form": form})
+
+from django.shortcuts import get_object_or_404
+
+def delete_movie(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    movie.delete()
+    return redirect("home")
+
+from django.shortcuts import get_object_or_404
+
+def edit_movie(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    if request.method == "POST":
+        form = MovieForm(request.POST, instance=movie)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = MovieForm(instance=movie)
+    return render(request, "movies/edit.html", {"form": form, "movie": movie})
+
